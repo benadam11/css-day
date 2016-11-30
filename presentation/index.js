6c107fd00faae19a544b7949918d6af92beb9b73
Ben Adam <bxadam@godaddy.com>
2016-11-29 22:21:22 -0700

    Adding a bunch of slides

--- presentation/index.js
+++ presentation/index.js
@@ -41,6 +41,11 @@ const images = {
   dots: require("../assets/dots.png"),
   charlie: require("../assets/charlie-day.png"),
   rich: require("../assets/rich.jpg"),
+  css: require("../assets/css-doctor.gif"),
+  lego: require("../assets/lego-castle.png"),
+  specificity: require("../assets/twitter-specificity.png"),
+  stats: require("../assets/twitter-stats.png"),
+  weave: require("../assets/woven-castle.png"),
   markdown: require("../assets/markdown.png")
 };
 
@@ -49,7 +54,7 @@ preloader(images);
 const theme = createTheme({
   primary: "#000",
   secondary: "#FF0053",
-},{
+}, {
   primary: 'Helvetica Neue'
 }) ;
 
@@ -78,14 +83,14 @@ export default class Presentation extends React.Component {
             <Slide transition={['slide']} bgColor="black">
               <Image src={images.charlie} margin="0px auto 40px" height="500px"/>
               <Text textColor='white' textSize='24px' italic >(Me attempting to explain FP + CSS in 30 minutes)</Text>
-            </Slide>
-            
-            <Slide transition={['slide']} bgColor="black">
-              <Heading size={2} textColor='white' italic >What is Good CSS?</Heading>
+            </Slide> 
+
+            <Slide transition={['slide']} bgColor="secondary">
+              <Heading size={6} textColor='white' italic >What is Good CSS?</Heading>
             </Slide>
 
             <Slide transition={['slide']} bgColor="black">
-                <Heading textAlign='left' size={4} textColor='secondary' italic >Good CSS is:</Heading>
+                <Heading textAlign='left' size={6} textColor='secondary' italic >Good CSS is:</Heading>
                 <List textColor='white'>
                   <Appear><ListItem>Simple</ListItem></Appear>
                   <Appear><ListItem>Predictable</ListItem></Appear>
@@ -94,7 +99,7 @@ export default class Presentation extends React.Component {
             </Slide>
 
             <Slide transition={['slide']} bgColor="black">
-                <Heading textAlign='left' size={4} textColor='white' italic >Bad CSS is: </Heading>
+                <Heading textAlign='left' size={6} textColor='white' italic >Bad CSS is: </Heading>
                 <List>
                   <Appear><ListItem>Complex</ListItem></Appear>
                   <Appear><ListItem>Unpredictable</ListItem></Appear>
@@ -104,7 +109,7 @@ export default class Presentation extends React.Component {
 
             <Slide transition={['slide']} bgColor="black">
               <Heading size={6} textColor="white" margin='40px 10px'>
-                Bad CSS is Complex
+                Problem #1 - Bad CSS is Complex:
               </Heading>
             </Slide>
 
@@ -125,94 +130,226 @@ export default class Presentation extends React.Component {
               </Markdown>
             </Slide>
 
-            <Slide transition={['fade', 'zoom']} bgColor="black">
-              <Text textColor='white' textSize='28px' italic >CSS is really easy to write, but very difficult to maintain.  </Text>
+            <Slide transition={["zoom", "fade"]} bgColor="primary">
+              <Layout>
+                <Fill><Image src={images.weave} height="300px"/></Fill>
+                <Fill><Image src={images.lego} height="300px"/></Fill>
+              </Layout>
+              <Text textColor='white' textSize='24px' italic>Which castle would you rather refactor?</Text>
             </Slide>
 
             <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-              <Text textAlign='center' textColor='white' lineHeight={2} textSize='28px'>
-                <span style={{fontWeight: '300'}}>CSS becomes complex over time because </span><span style={{fontWeight: 'bold', fontStyle: 'italic', fontSize:'30px'}}>it is much easier to write new CSS than it is to delete or refactor existing CSS.</span>
-              </Text>
+              <Heading size={6}textAlign='center' textColor='white' lineHeight={2} bold italic>
+                It is much easier to write new CSS than it is to delete or refactor existing CSS.
+              </Heading>
+            </Slide>
+
+            <Slide transition={['slide']} bgColor="black">
+                <Heading textAlign='left' size={6} textColor='secondary' italic >How does this happen?</Heading>
+                <List textColor='white'>
+                  <Appear><ListItem textSize='28px' margin='20px 0px'>Over time the codebase and / or team grows</ListItem></Appear>
+                  <Appear><ListItem textSize='28px' margin='20px 0px'>Developer aware than someone already wrote a class</ListItem></Appear>
+                  <Appear><ListItem textSize='28px' margin='20px 0px'>The existing class doesn’t do exactly what they need it to</ListItem></Appear>
+                  <Appear><ListItem textSize='28px'>Solution: Add more CSS</ListItem></Appear>
+              </List>
+            </Slide>
+
+
+            <Slide transition={['slide']} bgColor="black">
+              <Heading size={6} textColor="white" margin='40px 10px'>
+                Problem #2 - Bad CSS is Unpredictable:
+              </Heading>
+            </Slide>
+
+            <Slide transition={['fade', 'zoom']} bgColor="black">
+              <Image src={images.css} margin="0px auto 40px" height="500px"/>
+              <Text textColor='white' textSize='24px' italic>(Refactoring CSS IRL)</Text>
+            </Slide>
+
+            <Slide transition={['slide']} bgColor="black">
+                <Heading textAlign='left' size={6} textColor='secondary' italic >What makes CSS unpredictable? </Heading>
+                <List textColor='white'>
+                  <Appear><ListItem textSize='28px' margin='20px 0px'>Global scope</ListItem></Appear>
+                  <Appear><ListItem textSize='28px' margin='20px 0px'>Specificity / The Cascade</ListItem></Appear>
+                  <Appear><ListItem textSize='28px' margin='20px 0px'>Quirky inheritance</ListItem></Appear>
+              </List>
             </Slide>
 
             <Slide transition={["slide"]} bgColor="black">
-              <BlockQuote >
+              <BlockQuote>
                 <Quote textColor='white' textSize='63px'>“Simplicity is a prerequisite for reliability.”</Quote>
                 <span style={{position: 'relative', left: '28px' }}><Cite textColor='secondary'>Edsger Dijkstra</Cite></span>
               </BlockQuote>
             </Slide>
 
-            <Slide transition={['zoom','fade']} bgColor="primary" notes="<ul><li>talk about that</li><li>and that</li></ul>">
+            <Slide transition={['slide']} bgColor="secondary">
+              <Heading size={6} textColor='white' italic >How does Functional Programming address these issues?</Heading>
+            </Slide>
+
+            <Slide transition={["fade"]} bgColor="white">
+              <Appear>
+                <Heading size={6} textAlign='left' italic margin='40px 0px'>What is Functional Programming?</Heading>
+              </Appear>
+              <Appear>
+                <Text textAlign='left' textSize='24px' lineHeight={1.75}>
+                  Functional Programming is a programming paradigm—a style of building the structure 
+                  and elements of computer programs—that treats computation as the evaluation of <span 
+                  style={{fontWeight: 'bold', fontStyle: 'italic', fontSize:'26px'}}> mathematical functions
+                  and avoids changing-state and mutable data. </span>
+                  It is a declarative programming paradigm, which means programming is 
+                  done with expressions or declarations instead of statements.
+                </Text>
+              </Appear>
+            </Slide>
+            
+            <Slide transition={['slide']} bgColor="black">
+              <Heading size={6} textColor="secondary" margin='40px 10px'>
+                Principle 1 - Purity
+              </Heading>
+            </Slide>
+
+            <Slide transition={['zoom','fade']} bgColor="primary">
               <CodePane
                 lang="jsx"
-                source={require("raw!../assets/deck.example")}
+                source={require("raw!../examples/purity.example")}
                 margin="20px auto"
               />
             </Slide>
-            <Slide transition={["slide"]} bgDarken={0.25}>
-              <Appear fid="1">
-                <Heading size={1} caps fit textColor="primary">
-                  Full Width
-                </Heading>
-              </Appear>
-              <Appear fid="2">
-                <Heading size={1} caps fit textColor="secondary">
-                  Adjustable Darkness
-                </Heading>
-              </Appear>
-              <Appear fid="3">
-                <Heading size={1} caps fit textColor="primary">
-                  Background Imagery
-                </Heading>
-              </Appear>
+
+            <Slide transition={['zoom','fade']} bgColor="primary">
+              <CodePane
+                lang="jsx"
+                source={require("raw!../examples/purity2.example")}
+                margin="20px auto"
+              />
             </Slide>
-            <Slide transition={["zoom", "fade"]} bgColor="primary">
-              <Heading caps fit>Flexible Layouts</Heading>
-              <Layout>
-                <Fill>
-                  <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                    Left
-                  </Heading>
-                </Fill>
-                <Fill>
-                  <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                    Right
-                  </Heading>
-                </Fill>
-              </Layout>
-            </Slide>
-            
-            
-            <Slide transition={["slide", "fade"]} bgColor="primary">
-              <Heading caps fit size={1} textColor="tertiary">
-                Smooth
-              </Heading>
-              <Heading caps fit size={1} textColor="secondary">
-                Combinable Transitions
+
+            <Slide transition={['slide']} bgColor="black">
+              <Heading size={6} textColor="secondary" margin='40px 10px'>
+                Principle 2 - Immutability
               </Heading>
             </Slide>
-            <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-              <List>
-                <Appear><ListItem>Inline style based theme system</ListItem></Appear>
-                <Appear><ListItem>Autofit text</ListItem></Appear>
-                <Appear><ListItem>Flexbox layout system</ListItem></Appear>
-                <Appear><ListItem>React-Router navigation</ListItem></Appear>
-                <Appear><ListItem>PDF export</ListItem></Appear>
-                <Appear><ListItem>And...</ListItem></Appear>
+
+            <Slide transition={['zoom','fade']} bgColor="primary">
+              <CodePane
+                lang="jsx"
+                source={require("raw!../examples/immutability.example")}
+                margin="20px auto"
+              />
+            </Slide>
+
+            <Slide transition={['slide']} bgColor="black" notes='Highly recommend the Lee Byron talk on immutability'>
+              <Heading size={6} textColor='secondary' textAlign='left' italic >
+                Immutability is a virtuous constraint
+              </Heading>
+              <List textColor='white'>
+                <Appear><ListItem textSize='28px' margin='20px 0px'>Cheap equality checks on data</ListItem></Appear>
+                <Appear><ListItem textSize='28px' margin='20px 0px'>Data stays in sync with UI</ListItem></Appear>
+                <Appear><ListItem textSize='28px' margin='20px 0px'>Easier to persist data</ListItem></Appear>
               </List>
             </Slide>
-            <Slide transition={["slide"]} bgColor="primary">
-              <Heading size={1} caps fit textColor="tertiary">
-                Your presentations are interactive
+              
+            <Slide transition={['slide']} bgColor="black">
+              <Heading size={6} textColor="secondary" margin='40px 10px'>
+                Principle 3 - Composition
               </Heading>
-              <Interactive/>
             </Slide>
+
+            <Slide transition={['zoom','fade']} bgColor="primary">
+              <CodePane
+                lang="jsx"
+                source={require("raw!../examples/composition.example")}
+                margin="20px auto"
+              />
+            </Slide>
+            
+             <Slide transition={['slide']} bgColor="secondary">
+              <Heading size={6} textColor="white" margin='40px 10px'>
+                Applying these Principles to CSS
+              </Heading>
+            </Slide>
+
+            <Slide transition={['slide']} bgColor="black">
+              <Heading size={6} textColor="secondary" margin='40px 10px'>
+                Principle 1 - Purity
+              </Heading>
+            </Slide>
+
+            <Slide transition={['zoom','fade']} bgColor="primary">
+              <CodePane
+                lang="jsx"
+                source={require("raw!../examples/css-purity.example")}
+                margin="20px auto"
+              />
+            </Slide>
+
+            <Slide transition={['slide']} bgColor="black">
+              <Heading size={6} textColor="secondary" margin='40px 10px'>
+                Principle 2 - Immutability
+              </Heading>
+            </Slide>
+
+            <Slide transition={['fade', 'zoom']} bgColor="white">
+              <Heading size={6} textColor="black" textAlign='left' margin='80px 0px' italic>
+                CSS Stats for Twitter.com
+              </Heading>
+              <Image src={images.stats} margin="0px auto 40px" height="280px"/>
+            </Slide>
+
+            <Slide transition={['fade', 'zoom']} bgColor="white">
+              <Heading size={6} textColor="black" textAlign='left' margin='80px 0px' italic>
+               Specificity graph for Twitter.com
+              </Heading>
+              <Image src={images.specificity} margin="0px auto 40px" height="280px"/>
+            </Slide>
+
+            <Slide transition={['slide']} bgColor="black" notes='Highly recommend the Lee Byron talk on immutability'>
+              <Heading size={6} textColor='secondary' textAlign='left' italic >
+                How do we avoid overriding properties? 
+              </Heading>
+              <List textColor='white'>
+                <Appear><ListItem textSize='28px' margin='20px 0px'>Keep specificity flat by only using classes</ListItem></Appear>
+                <Appear><ListItem textSize='28px' margin='20px 0px'>Avoid inheritance and the cascade</ListItem></Appear>
+                <Appear><ListItem textSize='28px' margin='20px 0px'>Avoid property duplication across classes</ListItem></Appear>
+              </List>
+            </Slide>
+
+            <Slide transition={['slide']} bgColor="black">
+              <Heading size={6} textColor="secondary" margin='40px 10px'>
+                Principle 3 - Composition
+              </Heading>
+            </Slide>
+
+            <Slide transition={['zoom','fade']} bgColor="primary">
+              <CodePane
+                lang="jsx"
+                source={require("raw!../examples/css-composition.example")}
+                margin="20px auto"
+              />
+              <Appear>
+                <CodePane
+                  lang="jsx"
+                  source={require("raw!../examples/css-composition2.example")}
+                  margin="20px auto"
+                />
+              </Appear>
+            </Slide>
+
+            <Slide transition={['zoom','fade']} bgColor="primary">
+              <CodePane
+                lang="jsx"
+                source={require("raw!../examples/component.example")}
+                margin="20px auto"
+              />
+            </Slide>
+
             <Slide transition={["spin", "slide"]} bgColor="tertiary">
               <Heading size={1} caps fit lineHeight={1.5} textColor="primary">
                Thank you!
               </Heading>
               <Link href="http://www.twitter.com/benadam11" textColor='secondary'>@benadam11</Link>
             </Slide>
+
           </Deck>
         </Spectacle>
       </div>
