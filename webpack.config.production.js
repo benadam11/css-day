de8c67475e13b182969e174f3e090fbc4da14649
Benjamin Adam <benjamin.mark.adam@gmail.com>
2022-04-26 12:04:33 -0700

    Updates

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -39,13 +39,24 @@ module.exports = {
       }
     }, {
       test: /\.css$/,
-      loader: "style-loader!css-loader"
-    }, {
-      test: /\.(png|jpg|gif)$/,
-      loader: "url-loader?limit=8192"
+      loaders: ["style", "raw"],
+      include: __dirname
     }, {
       test: /\.svg$/,
-      loader: "url?limit=10000&mimetype=image/svg+xml"
+      loader: "url?limit=10000&mimetype=image/svg+xml",
+      include: path.join(__dirname, "assets")
+    }, {
+      test: /\.png$/,
+      loader: "url-loader?mimetype=image/png",
+      include: path.join(__dirname, "assets")
+    }, {
+      test: /\.gif$/,
+      loader: "url-loader?mimetype=image/gif",
+      include: path.join(__dirname, "assets")
+    }, {
+      test: /\.jpg$/,
+      loader: "url-loader?mimetype=image/jpg",
+      include: path.join(__dirname, "assets")
     }]
   }
 };
